47092e116a59159cc65b7a1ac383ce8aedeb680a
Zachary Schulz <zjschulz@gmail.com>
2019-08-20 02:14:56 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -4,7 +4,11 @@
 # in order to complete the rest of this course and be ready for the next course!
 def introduction(name)
   puts "Hi, my name is #{name}."
-
+end
+def introduction_with_language(name,language)
+  puts "Hi, my name is #{name} and I am learning to program #{language}."
+end
+def 
 
 
 
